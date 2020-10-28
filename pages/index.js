480830ae4cb45fc0ce682fde3b038dbb2db27ea4
Erich Rahm <erahm84@gmail.com>
2020-10-27 22:01:54 -0600

    creates skeleton for item component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import React from "react";
+import { Item } from "./components/Item";
+
 const Index = () => {
   return (
     <div>
-      <h1>this is the page</h1>
+      <div id="gear-wrapper">
+        <h1>Gear goes here</h1>
+        <Item slot="component-1"></Item>
+        <Item slot="component-2"></Item>
+        <Item slot="component-3"></Item>
+        <Item slot="component-4"></Item>
+        <Item slot="component-5"></Item>
+        <Item slot="component-6"></Item>
+        <Item slot="gun-1"></Item>
+        <Item slot="gun-2"></Item>
+        <Item slot="melee"></Item>
+        <Item slot="support"></Item>
+        <Item slot="ability-1"></Item>
+        <Item slot="ability-2"></Item>
+      </div>
     </div>
   )
 };
